44a55097fbbe788695ccf368cd7d347bfd5295cd
mphayden <mhayden.tech@gmail.com>
2017-06-15 20:58:54 -0400

    Modified the DB Connection to use AWS instead of localhost

--- MyGarage/MyGarage/DB/DBConnection.cs
+++ MyGarage/MyGarage/DB/DBConnection.cs
@@ -7,9 +7,11 @@ namespace MyGarage.DB
         public static SqlConnection GetConnection()
         {
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "localhost";
+            builder.DataSource = "mygarage.cezil3uadcim.us-east-2.rds.amazonaws.com,1433";
+            builder.UserID = "mygarage";
+            builder.Password = "8923dfahuweiQUWI";
             builder.InitialCatalog = "CS6920-team5";
-            builder.IntegratedSecurity = true;
+            builder.IntegratedSecurity = false;
             SqlConnection connection = new SqlConnection(builder.ConnectionString);
             return connection;
         }
