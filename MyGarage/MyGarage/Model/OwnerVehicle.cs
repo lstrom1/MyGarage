bc1b8f8aaa036ae21a526e8ece8e3e09eaece310
Troy Hilley <thilley1@my.westga.edu>
2017-06-20 19:53:50 -0400

    Created Model for OwnerVehicle which will hold the association from the owner to the vehicle.

---
+++ MyGarage/MyGarage/Model/OwnerVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGarage.Model
+{
+    class OwnerVehicle
+    {
+        public int ownerVehicleID { get; set;}
+        public int ownerID { get; set; }
+        public int vehicleID { get; set; }
+    }
+}
