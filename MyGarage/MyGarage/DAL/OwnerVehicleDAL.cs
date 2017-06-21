a2c518c03e7f10fdfea43a4a98e2731c081d8264
Troy Hilley <thilley1@my.westga.edu>
2017-06-20 20:38:24 -0400

    Added RemoveOwnerVehicle, which Disassociate an owner from a vehicle

--- MyGarage/MyGarage/DAL/OwnerVehicleDAL.cs
+++ MyGarage/MyGarage/DAL/OwnerVehicleDAL.cs
@@ -11,7 +11,12 @@ using System.Windows.Forms;
 namespace MyGarage.DAL
 {
     class OwnerVehicleDAL
-    {
+    {   
+        /// <summary>
+        /// Associates an Owner with a Vehicle
+        /// </summary>
+        /// <param name="newOwnerVehicle"></param>
+        /// <returns></returns>
         public static int CreateOwnerVehicle(OwnerVehicle newOwnerVehicle)
         {
             int exitStatus = 0;
@@ -49,6 +54,47 @@ namespace MyGarage.DAL
                 connection.Close();
             }
 
+            return exitStatus;
+        }
+        /// <summary>
+        /// Disassociates an Owner with a Vehicle
+        /// </summary>
+        /// <param name="ownerVehicle"></param>
+        /// <returns></returns>
+        public static int RemoveOwnerVehicle(OwnerVehicle ownerVehicle)
+        {
+            int exitStatus = 0;
+
+            SqlConnection connection = DBConnection.GetConnection();
+
+            string removeStatement = "DELETE FROM ownerVehicle " +
+                "WHERE (ownerID = @ownerID AND vehicleID = @vehicleID)";
+
+            SqlCommand removeCommand = new SqlCommand(removeStatement, connection);
+
+            try
+            {
+                connection.Open();
+                removeCommand.ExecuteNonQuery();
+                exitStatus = 0;
+            }
+            catch (SqlException ex)
+            {
+                exitStatus = 1;
+                StringBuilder errorDetails = new StringBuilder();
+                for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorDetails.Append("Index #" + i + "\n" +
+                        "Message: " + ex.Errors[i].Message + "\n" +
+                        "Error Number: " + ex.Errors[i].Number + "\n" +
+                        "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+                MessageBox.Show(errorDetails.ToString(), "SQL Exception");
+            }
+            finally
+            {
+                connection.Close();
+            }
             return exitStatus;
         }
 
