a9afea0212e308401f54b17c3a099ba72914d20d
Troy Hilley <thilley1@my.westga.edu>
2017-06-21 18:07:00 -0400

    Created ServiceCategory Model

---
+++ MyGarage/MyGarage/Model/ServiceCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGarage.Model
+{
+    class ServiceCategory
+    {
+        public int serviceCategoryID { get; set; }
+        public string categoryName { get; set; }
+        public int numberOfDays { get; set; }
+        public int mileageIntervals { get; set; }
+    }
+}
