e01aa8c382228e73f906c09b270d186dbf8ea448
mphayden <mhayden.tech@gmail.com>
2017-06-30 21:23:40 -0400

    Added ServiceRecord Model

---
+++ MyGarage/MyGarage/Model/ServiceRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyGarage.Model
+{
+    class ServiceRecord
+    {
+        // serviceRecordID type int in the DB primary key
+        public int serviceRecordID { get; set; }
+
+        // vehicleID type int in the DB
+        public int vehicleID { get; set; }
+
+        // dateOfService type date in the DB
+        public DateTime dateOfService { get; set; }
+
+        // mileage type int in the DB
+        public int mileage { get; set; }
+    }
+}
