615086b2fc761cdd69a5e4369d1f98423051f2d1
mphayden <mhayden.tech@gmail.com>
2017-07-02 20:09:20 -0400

    Iteration 2 Complete - All tests passing

--- MyGarage/MyGarage/View/AddServiceType.cs
+++ MyGarage/MyGarage/View/AddServiceType.cs
@@ -1,13 +1,6 @@
 ﻿using MyGarage.Controller;
 using MyGarage.Model;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MyGarage.View
