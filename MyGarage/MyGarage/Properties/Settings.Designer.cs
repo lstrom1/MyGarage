b870b4e8d210996e1565c490faaf0f8382ef377c
mphayden <mhayden.tech@gmail.com>
2017-07-15 15:26:21 -0400

    Created DataSets for each of the stored procedures

--- MyGarage/MyGarage/Properties/Settings.Designer.cs
+++ MyGarage/MyGarage/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MyGarage.Properties
-{
-
-
+namespace MyGarage.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=mygarage.cezil3uadcim.us-east-2.rds.amazonaws.com;Initial Catalog=CS6" +
+            "920-team5;Persist Security Info=True;User ID=mygarage")]
+        public string CS6920_team5_ConnectionString {
+            get {
+                return ((string)(this["CS6920_team5_ConnectionString"]));
             }
         }
     }
